a1d79e43aa49da1d6c97bd27dc32feb7fb0589a3
SHIntrestCROW1 <147429190+SHIntrestCROW1@users.noreply.github.com>
2024-02-05 10:51:51 -0500

    Create cubbyhole.rb
    cubbyhole 😆

---
+++ cubbyhole.rb
@@ -0,0 +1,47 @@
+class Cubbyhole {
+  public fun define {
+    var textclass = Handler();
+    val hieght = 42
+    val deepness = 43
+    var text = "Defnotran"
+    var bin = 0
+    towc()
+  }
+}
+  class Handler {
+    public fun defnotran {
+      var gender = "defnransexual"
+      val age = 50
+      var mm = false
+      var bin = 1
+      towc()
+    }
+    }
+      }
+    fun main(args: String<Array>) {
+      defnran() 
+      towc()
+    }
+    fun defnran(args: Cubbyhole<Handler>) {
+      var Instance = Cubbyhole()
+      var HandlerForInstance = Cubbyhole.textclass
+      printIn("Classes were invoked.")
+      HandlerForInstance.mm=true
+      this.Instance.textclass = ()!!toLine.3 => {true}
+      HandlerForInstance.age=45
+      
+      swap()
+      towc()
+    }
+    fun swap(args: Cubbyhole<Handler>):
+      var Instance = Cubbyhole()
+      var HandlerForInstance = Cubbyhole.textclass
+      printIn("Classes were invoked.")
+      Instance.bin=1
+      HandlerForInstance.bin=0
+      printIn("This swap was successfully completed.")
+      towc()
+  }  
+    fun towc() {
+      printIn("The operation successfully completed.)
+      }
