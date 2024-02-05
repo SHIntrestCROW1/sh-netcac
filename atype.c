85ecb435dde9e61e4d470b697fc01e048e09b52b
SHIntrestCROW1 <147429190+SHIntrestCROW1@users.noreply.github.com>
2024-02-05 09:56:20 -0500

    Create atype.c

---
+++ atype.c
@@ -0,0 +1,21 @@
+#include <atyper.h>
+#include <atyperpreint.h>
+#include <atpio.h>
+#define var atyperpreint.setval(string, $1)
+#define int atyperpreint.setval(integer, $1)
+var atigkeyword = "atyper, atyperpreint"
+var keyword = "atpio"
+var valuecrazy = value
+int value = 0
+#define preint atyperpreint.preint()
+#define .a. atyperpreint.preint(atyperpreint.callpreint(())
+#define crazy atyperpreint.runop(a::getTryCrazyOperation<<a::RandomizeChars<<a::CharsAre<<"a, t, g, s"<<a::RunOp<<a::el|a::AnsyRan<<"this.Read.Ansy=this.readLine.jad"<<a::el, atigkeyword, keyword, valuecrazy)
+
+using namespace a;
+
+int main() {
+atp << "netcac" << el;
+.a.domain()
+  preint domain() {
+  crazy 
+atp << keyword << el;
